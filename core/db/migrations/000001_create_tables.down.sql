c588c4093a11d27407099c80854302f99a457eea
DavidZaya21 <133985554+DavidZaya21@users.noreply.github.com>
2025-10-21 01:57:04 +0200

    schema

---
+++ core/db/migrations/000001_create_tables.down.sql
@@ -0,0 +1,9 @@
+DROP TABLE kainos_user cascade;
+
+DROP TABLE kainos_workflow cascade;
+
+DROP TABLE kainos_user_workflow cascade;
+
+DROP TABLE system_defined_analysis cascade;
+
+DROP TABLE kainos_user_analysis cascade;
--- core/db/query/user.sql
+++ core/db/query/user.sql
@@ -1,2 +1,25 @@
--- name: CreateAppUser :one
-INSERT  INTO app_users (clerk_id) VALUES  (clerk_id) RETURNING  *;
+-- name: CreateUser :one
+INSERT INTO kainos_user (clerk_id, first_name, email) VALUES (@clerk_id, @first_name, @email) returning *;
+
+-- name: CreateWorkflow :one
+INSERT INTO kainos_workflow (workflow_name, workflow_description, price) VALUES (@workflow_name, @workflow_description, @price) returning *;
+
+-- name: CreateUserWorkflow :one
+INSERT INTO kainos_user_workflow (workflow_id, customer_id, meta_data, status) VALUES (@workflow_id, @customer_id, @meta_data, @status) returning *;
+
+-- name: CreateSystemAnalysis :one
+INSERT INTO system_defined_analysis (analysis_type, description) VALUES (@analysis_type, @description) returning *;
+
+-- name: CreateUserAnalysis :one
+INSERT INTO kainos_user_analysis (description, s3_url, customer_id) VALUES (@description, @s3_url, @customer_id) returning *;
+
+-- name: GetWorkflow :many
+SELECT * from kainos_workflow;
+
+-- name: GetUserWorkflow :many
+SELECT workflow_id, workflow_name, meta_data, cron_time, status, kainos_user_workflow.created_at, kainos_user_workflow.updated_at
+        from kainos_user_workflow
+        join kainos_workflow
+            on kainos_user_workflow.workflow_id = kainos_workflow.id
+        join kainos_user
+        on kainos_user_workflow.customer_id = kainos_user.id;
